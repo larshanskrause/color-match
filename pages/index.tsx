59a10fe90fad681943902a08dc6fd3a5647d3eaf
Lars Krause <lars@arbitraer.de>
2022-03-03 21:34:39 +0100

    inital commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+    return <div className="w-40 h-40 bg-blue-500"></div>;
+};
+
+export default Home;
